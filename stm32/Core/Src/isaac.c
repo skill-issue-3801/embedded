b6c9071ea98f83fe8e1bdac0c1ee86d5ff8b0c3a
IsaacGraham128 <82865705+IsaacGraham128@users.noreply.github.com>
2023-09-02 22:48:48 +1000

    Minor Update
    Enabled keypresses based on serial input from nucleo board. Will need to migrate Python code to Pi and split current nucleo code into appropriate files (i.e., gpio, uart, etc...)

--- stm32/Core/Src/isaac.c
+++ stm32/Core/Src/isaac.c
@@ -16,6 +16,8 @@ uint32_t PB1_LastPress = 0;
 /* General serial debugging */
 char* led_noise = "Noise?\n\r";
 char* led_confirm = "switch!\n\r";
+char led_uart[1];
+uint8_t led_counter = 0;
 
 /*
  * This is my temporary playground for any functionality I wish to implement...
@@ -25,12 +27,16 @@ void HAL_GPIO_EXTI_Callback (uint16_t GPIO_Pin) {
 	uint32_t tick_now = HAL_GetTick();
 
 	if (GPIO_Pin == B1_Pin) {
-		if ((tick_now - PB1_LastPress) > 50) {
+		if ((tick_now - PB1_LastPress) > 200) {
 			PB1_LastPress = tick_now;
 			HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-			HAL_UART_Transmit(&huart2, (uint8_t*)led_confirm, 9, HAL_MAX_DELAY);
+//			HAL_UART_Transmit(&huart2, (uint8_t*)led_confirm, 9, HAL_MAX_DELAY);
+			led_uart[0] = led_counter + 48;
+			HAL_UART_Transmit(&huart2, (uint8_t*)led_uart, 2, HAL_MAX_DELAY);
+			led_counter++;
+			led_counter %= 10;
 		} else {
-			HAL_UART_Transmit(&huart2, (uint8_t*)led_noise, 9, HAL_MAX_DELAY);
+//			HAL_UART_Transmit(&huart2, (uint8_t*)led_noise, 9, HAL_MAX_DELAY);
 		}
 	}
 }
